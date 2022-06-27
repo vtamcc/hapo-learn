cbfd601389f04d2c3f49daa5b3fd25387f11e115
tuananhm2c <tuananhm2c@gmail.com>
2022-06-28 01:21:57 +0700

    #2 fix header,banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,20 @@
 @extends('layouts.app')
-
 @section('content')
-
 <section class="banner">
    <div class="banner-content">
         <div class="banner-header">
             <p class="banner-title">Learn Anytime, Anywhere
                 <span class="banner-title title-weight">at HapoLearn
-                    <img class="banner-image" src="{{ asset('images/Group 6.png') }}" 
+                    <img class="banner-image" src="{{ asset('images/Group6.png') }}" 
                     alt="HapoLearn Logo"> !
                 </span>
             </p>
         </div>
-
-        <!-- <div class="banner-logo>">
-            <p ></p>
-        </div> -->
-
         <div class="banner-text">
             <p class="paragrap">Interactive lessons, "on-the-go"
                practice, peer support.
             </p>
         </div>
-
         <button class="banner-btn"> Start Learning Now!</button>
    </div>
 </section>
