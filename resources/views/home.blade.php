f86ddb9fbd087ca9b13245f2e484a5f9d641fa44
tuananhm2c <tuananhm2c@gmail.com>
2022-06-29 09:30:04 +0700

    #13 courses

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,98 @@
 @extends('layouts.app')
-
 @section('content')
+<div class="cards container-fluid">
+    <div class="card-container container row">
+            <div class="card-col col-lg-4">
+                <div class="card-main card">
+                    <div class="card-image card-left">
+                        <img src="{{ asset('images/Rectangle7.png')}}" class="card-img-top" alt="" >
+                    </div>
+                    <div class="card-body">
+                        <p class="card-title">HTML/CSS/js Tutorial</p>
+                        <p class="card-text">I knew hardly anything about HTML,
+                                JS, and CSS before entering New Media. I had coded quite a bit,
+                                but never touched anything in regards to web development.</p>
+                        <button class="card-btn">Take This Course</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-col col-lg-4">
+                <div class="card-main card">
+                    <div class="card-image card-center">
+                        <img src="{{ asset('images/laravel-1-logo-black-and-white1.png') }}" class="card-img-top" alt="" >
+                    </div>
+                        <div class="card-body">
+                            <p class="card-title">LARAVEL Tutorial</p>
+                            <p class="card-text">I knew hardly anything about HTML,
+                                JS, and CSS before entering New Media. I had coded quite a bit,
+                                but never touched anything in regards to web development.</p>
+                            <button class="card-btn">Take This Course</button>
+                        </div>
+                    </div>
+                </div>
 
-<section class="banner">
-   <div class="banner-content">
-        <div class="banner-header">
-            <p class="banner-title">Learn Anytime, Anywhere
-                <span class="banner-title title-weight">at HapoLearn<img class="logo-image" src="{{ asset('images/Group 6.png') }}" alt="HapoLearn Logo"> !</span>
-            </p>
-        </div>
-
-        <!-- <div class="banner-logo>">
-            <p ></p>
-        </div> -->
-
-        <div class="banner-text">
-            <p class="paragrap">Interactive lessons, "on-the-go"
-               practice, peer support.
-            </p>
-        </div>
-
-        <button class="banner-btn"> Start Learning Now!</button>
-   </div>
-</section>
+            <div class="card-col col-lg-4">
+                <div class="card-main card">
+                    <div class="card-image card-right">
+                        <img src="{{asset('images/Rectangle15.png')}}" class="card-img-top" alt="" >
+                    </div>
+                    <div class="card-body">
+                        <p class="card-title">PHP Tutorial</p>
+                        <p class="card-text">I knew hardly anything about HTML,
+                                JS, and CSS before entering New Media. I had coded quite a bit,
+                                but never touched anything in regards to web development.</p>
+                        <button class="card-btn">Take This Course</button>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+<div class="other">
+    <p class="other-title">
+         Other courses
+    </p>
+</div>
+<div class="cards container-fluid">
+    <div class="card-container container row">
+            <div class="card-col col-lg-4">
+                <div class="card-main card">
+                    <div class="card-image card-left other-left">
+                        <img src="{{ asset('images/CSS.png')}}" class="card-img-top img-left" alt="" >
+                    </div>
+                    <div class="card-body">
+                        <p class="card-title">CSS Tutorial</p>
+                        <p class="card-text">I knew hardly anything about HTML, JS, and CSS
+                            before entering New Media,...</p>
+                        <button class="card-btn">Take This Course</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-col col-lg-4">
+                <div class="card-main card">
+                    <div class="card-image card-center other-center">
+                        <img src="{{ asset('images/Group.png') }}" class="card-img-top img-center" alt="" >
+                    </div>
+                        <div class="card-body">
+                            <p class="card-title">Ruby on rails Tutorial</p>
+                            <p class="card-text">I knew hardly anything about HTML, JS,
+                                and CSS before entering New Media,...</p>
+                            <button class="card-btn">Take This Course</button>
+                        </div>
+                    </div>
+                </div>
+            <div class="card-col col-lg-4">
+                <div class="card-main card">
+                    <div class="card-image card-right other-right">
+                        <img src="{{asset('images/java1.png')}}" class="card-img-top" alt="" >
+                    </div>
+                    <div class="card-body">
+                        <p class="card-title">Java Tutorial</p>
+                        <p class="card-text">I knew hardly anything about HTML, JS,
+                                and CSS before entering New Media,...</p>
+                        <button class="card-btn">Take This Course</button>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
 @endsection
